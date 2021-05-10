1edaeb197acc95194319f78fb1083d9d527a50df
Anna Syharevskaya <ann.syharevskaya@gmail.com>
2021-05-10 17:38:02 +0300

    clean code for copytext()

--- js/custom.js
+++ js/custom.js
@@ -6,30 +6,10 @@ document.addEventListener('DOMContentLoaded', () => {
   });
 });
 
-var text = document.getElementById("copyText");
-var btn = document.getElementById("copyButton");
-
-var text1 = document.getElementById("copyText1");
-var btn1 = document.getElementById("copyButton1");
-
-function copytext() {
-  console.log("Button clicked.");
-
-  var copyText = document.getElementById("copyText");
-  console.log('copyText = ',copyText);
+function copytext(id) {
+  var copyText = document.getElementById(id);
   copyText.select();
   document.execCommand("copy");
-  // alert("Copied the text: " + copyText.value);
-};
-
-function copytext1() {
-  console.log("Button clicked.");
-
-  var copyText = document.getElementById("copyText1");
-  console.log('copyText1 = ',copyText1);
-  copyText.select();
-  document.execCommand("copy");
-  // alert("Copied the text: " + copyText1.value);
 };
 
 
