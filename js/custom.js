6b2a51a49a95c0ac2b0993b6f21f289950ab4254
Anna Syharevskaya <ann.syharevskaya@gmail.com>
2021-05-10 13:32:22 +0300

    same style edits for mobile menu

--- js/custom.js
+++ js/custom.js
@@ -3,7 +3,16 @@ document.addEventListener('DOMContentLoaded', () => {
   var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
   var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
     return new bootstrap.Tooltip(tooltipTriggerEl)
-  })
+  });
 });
+
+var text = document.getElementById("copyText");
+var btn = document.getElementById("copyButton");
+
+function copytext() {
+  console.log("Button clicked.");
+  text.select();
+  document.execCommand("copy");
+};
 
 
